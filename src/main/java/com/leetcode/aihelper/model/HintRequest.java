75c0e0f121be3a272e2f550644ff7c14588bdb68
ksp2701 <ketanpatil2701@gmail.com>
2025-08-09 00:30:18 +0530

    Added main Spring Boot class and fixed package structure

--- src/main/java/com/leetcode/aihelper/model/HintRequest.java
+++ src/main/java/com/leetcode/aihelper/model/HintRequest.java
@@ -3,6 +3,12 @@ package com.leetcode.aihelper.model;
 public class HintRequest {
     private String problemTitle;
 
+    public HintRequest() {}
+
+    public HintRequest(String problemTitle) {
+        this.problemTitle = problemTitle;
+    }
+
     public String getProblemTitle() {
         return problemTitle;
     }
--- src/main/java/com/leetcode/aihelper/model/HintResponse.java
+++ src/main/java/com/leetcode/aihelper/model/HintResponse.java
@@ -3,6 +3,8 @@ package com.leetcode.aihelper.model;
 public class HintResponse {
     private String hint;
 
+    public HintResponse() {}
+
     public HintResponse(String hint) {
         this.hint = hint;
     }
